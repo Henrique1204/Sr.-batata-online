86a618c050ec295070f262f1d2167c2ed19511f4
paulo <pauloh16.pdr@gmail.com>
2019-05-02 18:40:13 -0300

    Adicionar lógica de avançar

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-const   
+const inicial = document.querySelector("#inicial");
+const introducao = document.querySelector("#introducao");
+const pergunta1 = document.querySelector("#p1");
+
+document.getElementById("comeca").addEventListener("click", function(){
+    inicial.style.display = "none";
+    introducao.style.display = "flex";
+});
+
+document.getElementById("avancar").addEventListener("click", function(){
+    introducao.style.display = "none";
+    pergunta1.style.display = "flex";
+});
